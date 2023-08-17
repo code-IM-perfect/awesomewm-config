3515c0eefec7bbde63d60b19c9d628a43b896c90
Harshit Tomar <harshitt345@gmail.com>
2023-08-17 19:14:51 +0530

    adding functions

---
+++ cool/create.lua
@@ -0,0 +1,22 @@
+------------------------- Widget Making Functons ---------------------------------------------
+local Cool = {}
+local wibox = require("wibox")
+local gears = require("gears")
+local naughty = require("naughty")
+
+Cool.widgetSpacer = function(width)
+	local hh = {
+		widget = wibox.container.background,
+		forced_width = width,
+	}
+
+	return hh
+end
+
+Cool.twoSolidColors = function (x0,y0,x1,y1,ratio,color1,color2)
+	-- return gears.color.create_linear_pattern("0,0:0,35:0,#FF0000:0.4999,#FF0000:0.5,#0000FF")
+	return gears.color.create_linear_pattern(string.format("%d,%d:%d,%d:%f,%s:%f,%s", x0, y0, x1, y1, ratio-0.0001, color1 ,ratio, color2))
+	-- naughty.notify{text=string.format("%d,%d:%d,%d:%f,%s:%f,%s", x0, y0, x1, y1, ratio-0.0001, color1 ,ratio, color2), timeout=0}
+end
+
+return Cool
---
+++ cool/general.lua
@@ -0,0 +1,25 @@
+----------------------------------- General Functions --------------------------------------------
+local Cool = {}
+
+-- math.randomseed(os.clock() * os.time() * math.random(247598))
+
+
+Cool.splitString = function(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local t = {}
+	for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+		table.insert(t, str)
+	end
+	return t
+end
+
+Cool.shuffleTable = function(t)
+	for i = #t, 2, -1 do
+		local j = math.random(i)
+		t[i], t[j] = t[j], t[i]
+	end
+end
+
+return Cool
