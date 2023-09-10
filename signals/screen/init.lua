e7ddf05c505871d8fb229cd611243648c25044e4
Harshit Tomar <harshitt345@gmail.com>
2023-09-10 13:02:44 +0530

    theme wallpaper to fill instead of fit

--- signals/screen/init.lua
+++ signals/screen/init.lua
@@ -1,28 +1,31 @@
 local awful = require'awful'
 local beautiful = require'beautiful'
-local wibox = require'wibox'
+-- local wibox = require'wibox'
+local gears = require'gears'
 
 local vars = require'config.vars'
 local widgets = require'widgets'
 
-screen.connect_signal('request::wallpaper', function(s)
-   awful.wallpaper{
-      screen = s,
-      widget = {
-         {
-            image     = beautiful.wallpaper,
-            upscale   = true,
-            downscale = true,
-            widget    = wibox.widget.imagebox,
-         },
-         valign = 'center',
-         halign = 'center',
-         tiled = false,
-         widget = wibox.container.tile,
-      }
+-- screen.connect_signal('request::wallpaper', function(s)
+--    awful.wallpaper{
+--       screen = s,
+--       widget = {
+--          {
+--             image     = beautiful.wallpaper,
+--             upscale   = true,
+--             downscale = true,
+--             widget    = wibox.widget.imagebox,
+--          },
+--          valign = 'center',
+--          halign = 'center',
+--          tiled = false,
+--          widget = wibox.container.tile,
+--       }
 
-   }
-end)
+--    }
+-- end)
+
+gears.wallpaper.maximized(beautiful.wallpaper)
 
 screen.connect_signal('request::desktop_decoration', function(s)
    awful.tag(vars.tags, s, awful.layout.layouts[1])
