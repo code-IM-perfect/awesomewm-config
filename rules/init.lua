d2510d431379b21585c41a1f24342a48213a7322
Harshit Tomar <harshitt345@gmail.com>
2023-09-29 14:09:00 +0530

    minor change in comment

--- rules/init.lua
+++ rules/init.lua
@@ -40,7 +40,7 @@ ruled.client.connect_signal('request::rules', function()
          role = {
             'AlarmWindow',   -- Thunderbird's calendar.
             'ConfigManager', -- Thunderbird's about:config.
-            'pop-up',        -- e.g. Google Chrome's (detached) Developer Tools.
+            'pop-up',        -- e.g. Firfox's (detached) Developer Tools.
          }
       },
       properties = { floating = true }
