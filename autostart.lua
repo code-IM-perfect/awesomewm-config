bd283904e6f86cbec6cc034ce4d8e2b0261b4685
Harshit Tomar <harshitt345@gmail.com>
2023-09-29 13:41:43 +0530

    start picom as a daemon

--- autostart.lua
+++ autostart.lua
@@ -2,5 +2,6 @@ awful = require("awful")
 
 awful.spawn.with_shell("klipper")
 awful.spawn.with_shell("playerctld daemon")
-awful.spawn.single_instance("picom")
+-- awful.spawn.single_instance("picom -b --experimental-backends")
+awful.spawn.single_instance("picom -b")
 awful.spawn.with_shell("/usr/lib/polkit-1/polkit-agent-helper-1")
