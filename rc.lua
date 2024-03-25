7f0f2f1de06b1f6cd4daec95bf2b32dd7b9fc982
Harshit Tomar <harshitt345@gmail.com>
2024-03-25 22:59:44 +0530

    show titlebar only on floating windows

--- rc.lua
+++ rc.lua
@@ -20,10 +20,42 @@ require 'signals'
 
 require 'timers'
 
-local modify = require'cool.modify'
+local modify = require 'cool.modify'
 -- require 'naughty'.notify { text = require 'gears'.filesystem.get_configuration_dir() }
 modify.changeWallpaper("current")
 
 modify.updateVolume()
 
 require 'autostart'
+
+
+
+-- turn titlebar on when client is floating
+-------------------------------------------------------------------------------
+client.connect_signal("property::floating", function(c)
+	if c.floating and not c.requests_no_titlebar and c.class ~= "firefox" then
+		awful.titlebar.show(c)
+	else
+		awful.titlebar.hide(c)
+	end
+end)
+-- turn tilebars on when layout is floating
+-------------------------------------------------------------------------------
+
+
+-- Not so good tries
+
+-- client.connect_signal("property::floating", function(c)
+--     if c.floating then
+-- 		awful.titlebar.show(c)
+--     else
+-- 		awful.titlebar.show(c)
+--     end
+-- end)
+
+--   awful.tag.attached_connect_signal(nil, "property::layout", function (t)
+-- 	local float = t.layout.name == "floating"
+-- 	for _,c in pairs(t:clients()) do
+-- 	  c.floating = float
+-- 	end
+--   end)
