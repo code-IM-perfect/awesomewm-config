d74ea715978e8589e52699d116ac110de7f2eedb
Harshit Tomar <harshitt345@gmail.com>
2025-03-09 16:33:11 +0530

    Modify code to follow best practices

--- bindings/client/key.lua
+++ bindings/client/key.lua
@@ -1,6 +1,6 @@
-local awful = require'awful'
+local awful = require("awful")
 
-local mod = require'bindings.mod'
+local mod = require("bindings.mod")
 
 client.connect_signal('request::default_keybindings', function()
    awful.keyboard.append_client_keybindings{
