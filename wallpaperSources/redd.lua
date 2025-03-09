6db99d6862158f29599b4ed01511d38d3822997b
Harshit Tomar <harshitt345@gmail.com>
2025-03-09 16:40:12 +0530

    Add midjourney to redd wallpaper sources

--- wallpaperSources/redd.lua
+++ wallpaperSources/redd.lua
@@ -1,6 +1,8 @@
 return {
-    "~/Harshit_Work/Funny_Stuff/z_wallpaper",
-    "~/Harshit_Work/Funny_Stuff/z_krita",
+	"~/Harshit_Work/Funny_Stuff/z_wallpaper",
+	"~/Harshit_Work/Funny_Stuff/z_krita",
+	"~/Harshit_Work/Funny_Stuff/z_midjourney",
 }
 
 ---------awk '{ print "\""$0"\","}'
+
