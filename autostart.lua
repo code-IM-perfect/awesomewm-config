024717c6534572050af04b4c250abb8ae6914783
Harshit Tomar <harshitt345@gmail.com>
2025-07-30 03:02:07 +0530

    Add autorandr to autostart

--- autostart.lua
+++ autostart.lua
@@ -1,5 +1,9 @@
 awful = require("awful")
 
+awful.spawn.with_shell("autorandr -c")
+-- awful.spawn.with_shell(
+-- 	"bash -c \"stdbuf -oL libinput debug-events | grep --line-buffered -E '^ event[0-9]+\\s+SWITCH_TOGGLE\\s' | while read line; do autorandr --change --default default; done\""
+-- )
 awful.spawn.with_shell("klipper")
 awful.spawn.with_shell("playerctld daemon")
 -- awful.spawn.single_instance("picom -b --experimental-backends")
